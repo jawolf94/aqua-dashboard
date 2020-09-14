fc44868b534ec9785f59b72ed82295f54f72e6a7
jawolf17 <jawolf17@gmail.com>
2020-09-14 17:00:27 -0400

    Create API to read temperature data from flask backend * Implemented Temperature model. * Implemented TemperatureApiService to retreive data. * Updated App component to display temperature data.

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Subscription} from 'rxjs/Subscription';
+import {TemperaturesApiService} from './temperatures/temperatures-api.service';
+import {Temperature} from './temperatures/temperature.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'UI';
+export class AppComponent implements OnInit, OnDestroy {
+    title = 'UI';
+    temperatureListSubs: Subscription;
+    temperatureList: Temperature[];
+
+    constructor(private temperaturesApi: TemperaturesApiService){
+
+    }
+
+    ngOnInit() {
+      this.temperatureListSubs = this.temperaturesApi
+        .getTemperatures()
+        .subscribe(res => {
+          this.temperatureList = res;
+        },
+        console.error
+        );
+    }
+
+    ngOnDestroy(){
+      this.temperatureListSubs.unsubscribe();
+    }
 }
--- UI/src/app/temperatures/temperatures-api.service.ts
+++ UI/src/app/temperatures/temperatures-api.service.ts
@@ -1,1 +1,23 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Observable} from '@rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import {API_URL} from '../env';
+import {Temperature} from './temperature.model';
+
+@Injectable()
+export class TemperaturesApiService {
+    constructor(private http: HttpClient){
+
+    }
+
+    private static _handleError(err: HttpErrorResponse | any){
+        return Observable.throw(err.message || 'Error: Unable to complete request.');
+    }
+
+    getTemperatures(): Observable<Temperature[]> {
+        return this.http
+            .get('${API_URL}/temperatures')
+            .catch(TemperaturesApiService._handleError);
+    }
+}
