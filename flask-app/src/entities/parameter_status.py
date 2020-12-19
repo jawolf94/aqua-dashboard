208d6b6580fd2f79867f8b0b49ab707f3fbe93e4
jawolf17 <jawolf17@gmail.com>
2020-12-18 20:09:15 -0500

    Pushing missing parameter_status file

--- flask-app/src/entities/parameter_status.py
+++ flask-app/src/entities/parameter_status.py
@@ -11,11 +11,11 @@ class ParameterStatus(Base, Entity):
 
     # Column definitions
     reading_id = Column(Integer, primary_key=True)
-    ammonia_ppm = Column(Boolean)
-    nitrite_ppm = Column(Boolean)
-    nitrate_ppm = Column(Boolean)
-    ph = Column(Boolean)
-    temperature = Column(Boolean)
+    ammonia_ppm = Column(Boolean, nullable=False)
+    nitrite_ppm = Column(Boolean, nullable=False)
+    nitrate_ppm = Column(Boolean, nullable=False)
+    ph = Column(Boolean, nullable=False)
+    temperature = Column(Boolean, nullable=False)
 
     def __init__(self, reading_id, ammonia_ppm, nitrite_ppm, nitrate_ppm,  ph, temperature):
         # Init parent class
